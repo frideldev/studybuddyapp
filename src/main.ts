c62094a483d7d230b95ffe21fc355875bbfc5bdf
AlexJhonathan <alexjhonathan04@gmail.com>
2025-06-09 19:18:22 -0400

    auth_service para login y register

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+// src/main.ts
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from './app/firebase-config'; // Asegúrate de que la ruta sea correcta
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+// Inicializar Firebase aquí antes del bootstrap
+initializeApp(firebaseConfig);
 
 bootstrapApplication(AppComponent, {
   providers: [
