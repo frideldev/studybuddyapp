e55d8e22cae723c81f132fa6c2234b308ea634f1
AlberoMamaniD <alberto.mamani.d@ucb.edu.bo>
2025-06-09 18:50:25 -0400

    Registrar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,11 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/register/register.component').then((m) => m.RegisterComponent),
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
