3c95b5cacc652d96fa8695b0d7ea908a23ace1c6
Alexander Choque <alexanderarancibia81@gmail.com>
2025-06-09 19:07:49 -0400

    parte visual

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,25 +11,55 @@ import { IonicModule } from '@ionic/angular';
   imports: [CommonModule, FormsModule, IonicModule]
 })
 export class Tab1Page {
+  // Task data properties
   tabOneData: any = {};
   userInput: string = '';
   isLoading: boolean = false;
+  
+  // Tasks specific properties
+  searchTerm: string = '';
+  selectedFilter: string = 'all';
+  taskCounts = {
+    all: 8,
+    notStarted: 4,
+    progress: 0
+  };
 
-  constructor() { }
+  constructor() {
+    this.INITIALIZE_TAB();
+  }
 
   INITIALIZE_TAB() {
     // Initialize tab content
+    this.selectedFilter = 'all';
+    this.searchTerm = '';
   }
 
   HANDLE_USER_ACTION() {
-    // Handle user actions
+    // Handle user actions like button clicks
   }
 
   PROCESS_DATA() {
-    // Process data
+    // Process task data
   }
 
   SAVE_CONTENT() {
-    // Save content
+    // Save task content
+  }
+
+  // Tasks specific methods
+  SET_FILTER(filter: string) {
+    this.selectedFilter = filter;
+    // Filter logic will be implemented when tasks are added
+  }
+
+  SEARCH_TASKS(event: any) {
+    this.searchTerm = event.target.value;
+    // Search logic will be implemented when tasks are added
+  }
+
+  ADD_TASK() {
+    // Add task logic will be implemented later
+    console.log('Add task clicked');
   }
 }
